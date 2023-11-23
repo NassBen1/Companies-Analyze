157d4adbea0c1dad3d228034737ca87db6c9fa77
Nassim Benhamou <nassim.benhamou@etu.imt-nord-europe.fr>
2023-11-23 20:49:45 +0100

    Avancée sur Exercice 2 avec affichage PyPlot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import plotly.express as px
 import os
 import re
 
@@ -24,7 +25,22 @@ def OpenDocsAndTagging(link):
 
     return merged_df
 
+#Fonction qui pour chaque année de création, le nombre de produits encore actif
+def CountActivePerYear(df) :
+    # Filtrer les produits encore actifs
+    active_products = df[df["Status"] == "Active"]
 
+    # Compter le nombre de produits actifs pour chaque année
+    active_products_count = active_products.groupby("Launch Year").size()
+    active_products_count = active_products_count.reset_index(name='Number of Active Products')
+
+    return active_products_count
+
+#Affichage En barre d'active_products
+def VisualisationEnBarre(data):
+
+    fig = px.bar(data, x='Launch Year', y='Number of Active Products', labels={'Launch Year': 'Année de Lancement', 'Number of Active Products': 'Nombre de Produits Actifs'})
+    fig.show()
 
 
 
@@ -41,6 +57,12 @@ if __name__ == "__main__" :
     # Afficher le type des valeurs des colonnes
     print("Voici le type des valeurs des colonnes:\n")
     print(df.dtypes)
+
+    #Compter le nombre de produits actifs pour chaque année de lancement. (Le mieux ici serait une visualisation que je vais effectuer en utilisant la librairie Plotly)
+    active_products = CountActivePerYear(df)
+    VisualisationEnBarre(active_products)
+
+
 
 
 
